257375dfd5048790dd78508915d383e5a655169e
Jan Gieseler <jan.gieseler@utu.fi>
2025-06-27 15:09:25 +0300

    change folder structure

--- tests/test.py
+++ tests/test.py
@@ -21,10 +21,10 @@ Install dependencies for tests:
 pip install flake8 pytest pytest-doctestplus pytest-cov pytest-mpl
 
 To create/update the baseline images, run the following command from the base package dir:
-pytest --mpl-generate-path=vda/tests/baseline vda/tests/test.py
+pytest --mpl-generate-path=tests/baseline tests/test.py
 
 To run the tests locally, go to the base directory of the repository and run:
-pytest -rP --mpl --mpl-baseline-path=baseline --mpl-baseline-relative --mpl-generate-summary=html --cov=vda vda/tests/test.py
+pytest -rP --mpl --mpl-baseline-path=baseline --mpl-baseline-relative --mpl-generate-summary=html tests/test.py
 """
 
 
